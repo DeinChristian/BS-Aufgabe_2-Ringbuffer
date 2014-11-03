4b041231c06db3787540d849bd70612751725a9a
Eike-Christian Ramcke <dein.christian@googlemail.com>
2014-11-03 22:03:32 +0100

    Init

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: Louisa Spahl
+ *
+ * Created on 3. November 2014, 21:05
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Main
+ */
+int main(int argc, char** argv) {
+    printf("Hallo Welt!");
+    return (EXIT_SUCCESS);
+}
+
