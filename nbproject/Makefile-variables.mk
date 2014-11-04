fa356984cf16f15f54131f4bf0cb994ffbe9c7d9
Eike-Christian Ramcke <dein.christian@googlemail.com>
2014-11-04 13:53:19 +0100

    Add project files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=bs-aufgabe_2-ringbuffer
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/bs-aufgabe_2-ringbuffer
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=bs-aufgabe2-ringbuffer.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/bs-aufgabe2-ringbuffer.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=bs-aufgabe_2-ringbuffer
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/bs-aufgabe_2-ringbuffer
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=bs-aufgabe2-ringbuffer.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/bs-aufgabe2-ringbuffer.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
