436e496aeaebc92a1f0eb8c62983d8de395f8c34
Eike-Christian Ramcke <dein.christian@googlemail.com>
2014-11-04 08:18:25 +0100

    Vorgegebene Implementations eingetragen

--- main.c
+++ main.c
@@ -33,8 +33,8 @@ typedef struct {
 rb x = {{0}, NULL, NULL, 0}; // Leerer Ringpuffer x
 rb *p_rb = &x; // Pointer auf Ringpuffer x
 
-#define p_start (int *)(p_rb -> buffer) // Pointer auf erstes Element im Buffer
-#define p_end (int *)((p_rb -> buffer) + MAX-1) // Pointer auf letztes Element im Buffer
+#define p_start (int *)(p_rb->buffer) // Pointer auf erstes Element im Buffer
+#define p_end (int *)((p_rb->buffer) + MAX-1) // Pointer auf letztes Element im Buffer
 
 void* p_1_w(void *pid); // Prozess 1
 void* p_2_w(void *pid); // Prozess 2
@@ -45,24 +45,117 @@ void* control(void *pid); // Control-Prozess
  * Main
  * @param argc
  * @param argv
- * @return 
+ * @return EXIT_SUCCESS
  */
 int main(int argc, char** argv) {
-    int i;
     pthread_t threads[4];
     
     printf("Start der Simulation! \n");
     
-    p_rb -> p_in = p_start; // Setzen von p_in Pointer im Buffer auf p_start
-    p_rb -> p_out = p_start; // Setzen von p_out Pointer im Buffer auf p_start
-    p_rb -> count = 0; // Setzen von count im Buffer auf 0
+    //...
+    
+    p_rb->p_in = p_start; // Setzen von p_in Pointer im Buffer auf p_start
+    p_rb->p_out = p_start; // Setzen von p_out Pointer im Buffer auf p_start
+    p_rb->count = 0; // Setzen von count im Buffer auf 0
     
     printf("Counter (im Buffer) ist %d. \n", p_rb -> count);
     
     // Threads starten
-    pthread_create(&threads[0], NULL, start_routine(), )
+    pthread_create(&threads[0], NULL, start_routine(), (void *)thread_id);
+    pthread_create(&threads[1], NULL, start_routine(), (void *)&thread_id[1]);
+    pthread_create(&threads[2], NULL, start_routine(), (void *)&thread_id[2]);
+    
+    //...
+    
+    // Threads joinen bzw. warten, bis alle Threads terminieren
+    for(int i = 0; i < 4; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    printf("Ende (nach dem Join der Threads). \n");
     
     return (EXIT_SUCCESS);
+}
+
+/**
+ * Schreiben
+ * @param pid Prozess-ID
+ * @return NULL
+ */
+void* write_c(void *pid) {
+    int i = 0;
+    int z_var = 0;
+    
+    printf("Start: Schreiben von Prozess %d: \n", *(int*)pid);
+    
+    while(1) {
+        i++;
+        z_var++;
+        
+        if(z_var > 9) {
+            z_var = 0;
+        }        
+        pthread_mutex_lock(&rb_mutex);
+        
+        // Prüfung, ob der Ringbuffer voll ist
+        while(p_rb->p_in == p_rb->p_out && p_rb->count == MAX) {
+            printf("Ringpuffer voll! Warten ... \n");
+            pthread_cond_wait(&not_full_condvar, &rb_mutex);
+            printf("Prozess %d ist aufgewacht. Count: %d \n", *(int*)pid, p_rb->count);            
+        }
+        
+        *(p_rb->p_in) = z_var;
+        (p_rb->p_in)++;
+        if(p_rb->p_in > p_end) {
+            p_rb->p_in = p_start;
+        }
+        (p_rb->count)++;
+        if(p_rb->count != 0) {
+            printf("Buffer signalisiert, dass er nicht leer ist. \n");
+            pthread_cond_signal(&not_empty_condvar);            
+        }
+        
+        pthread_mutex_unlock(&rb_mutex);
+        sleep(1);
+    }
+    return (NULL);
+}
+
+/**
+ * Lesen
+ * @param pid
+ * @return NULL
+ */
+void* read_rb(void *pid) {
+    int i = 0;
+    
+    printf("Start: Lesen von Prozess %d: \n", *(int*)pid);
+    
+    while(1) {
+        i++;
+        pthread_mutex_lock(&rb_mutex);
+        
+        // Prüfung, ob der Ringbuffer leer ist
+        while(p_rb->count == 0) {
+            printf("Ringpuffer leer! Warten ... \n");
+            pthread_cond_wait(&not_empty_condvar, &rb_mutex);
+            printf("Prozess %d ist aufgewacht. Count: %d \n", *(int*)pid, p_rb->count);            
+        }
+        
+        (p_rb->count)--;
+        printf("Älteres Zeichen ausgegeben: %d: \n", *(p_rb->p_out));
+        (p_rb->p_out)++;
+        if(p_rb->p_out > p_end) {
+            p_rb->p_out = p_start;
+        }
+        if(p_rb->count <= MAX) {
+            printf("Buffer signalisiert, dass er nicht voll ist. \n");
+            pthread_cond_signal(&not_full_condvar);
+        }
+        pthread_mutex_unlock(&rb_mutex);
+        sleep(1);
+    }
+    
+    return (NULL);
 }
 
 /**
